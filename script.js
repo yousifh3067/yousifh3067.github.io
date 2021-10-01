3f5b98ae8c20a174fe03775585f571daeb371662
yousifh3067 <91745102+yousifh3067@users.noreply.github.com>
2021-10-01 11:16:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Yousif Hawash"
+let userName = "Yousifh3067";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
